7e2add80433a24b90b6ba7212864f27da3ad1207
Jagdip Purewal <jagdip.purewal@engineering.digital.dwp.gov.uk>
2024-07-19 09:56:10 +0100

    [feat-test1907]
    GitOrigin-RevId: 848c648d6713d47d3d619b79f3de37ff3ab9f35c

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,5 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+require('./routes/alpha/iteration-1')(router)
